2f6877e88697c428dfc0a0300e3679862b0bf30c
joao victor <86917818+nova2DOT@users.noreply.github.com>
2022-01-21 08:23:26 -0300

    Add files via upload

---
+++ JavaSwingEx/src/javaswingex/JavaSwingEx.java
@@ -0,0 +1,11 @@
+package javaswingex;
+
+public class JavaSwingEx {
+
+    public static void main(String[] args) {
+        CadastroUsuario fm = new CadastroUsuario();
+        fm.setVisible(true);
+
+    }
+
+}
